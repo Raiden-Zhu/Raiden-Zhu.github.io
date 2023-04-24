64ac086d8178677844aaea2498884534be77d083
Tongtian Zhu <107566516+Raiden-Zhu@users.noreply.github.com>
2023-04-24 16:22:11 +0800

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
         $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
     });
     $('a.tldr').click(function() {
         $(this).parent().parent().find(".tldr.hidden").toggleClass('open');
@@ -11,6 +12,7 @@ $(document).ready(function() {
 $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
         $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+        $(this).parent().parent().find(".tldr.hidden.open").toggleClass('open');
     });
     $('a').removeClass('waves-effect waves-light');
 });
